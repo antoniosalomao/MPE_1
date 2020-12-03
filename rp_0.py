3a248807f40f5e7b35761f26649c52c01db3f592
Antônio Salomão <a.xsalomao@outlook.com>
2020-12-02 23:55:10 -0300

    Add files via upload

--- rp_0.py
+++ rp_0.py
@@ -31,10 +31,8 @@ def get_ret_vol_mvutility(weights, d_ra, returns, covar):
     '''
     d_ra: risk aversion parameter
     d_ra --> infinity --> minimum variance portfolio
-
     *** arg max problem --> expected_return - d_ra * port_variance
     *** arg min problem --> (0.5)*port_variance - d_ra^(-1) * expected_return
-
     '''
     weights = np.array(weights)
     expected_return = returns@weights*252
@@ -98,24 +96,27 @@ for i, row in df_exp_ret.iterrows():
 
 g_cons = ({'type': 'eq',
            'fun': target_vol(sigma=0.25, returns_vol_arg=df_exp_ret['Expected Return'], covar_vol_arg=df_covar)})
-h_cons = ({'type': 'ineq',
+h_cons = ({'type': 'eq',
            'fun': check_sum(C=1)})
+
+LB_i = 0
+UB_i = 1
 
 eq_mvutility_L = []
 s_n = 0
-n_trials = 10
+n_trials = 20
 for i in range(n_trials):
     '''
     Attempting to find true global minima via iteration
     '''
-    init_weights = np.random.uniform(low=0, high=0.15, size=(len(eq_univ)))
-    G_bounds = get_bounds(weights=init_weights, LB=0, UB=0.15)
+    init_weights = np.random.uniform(low=LB_i, high=UB_i, size=(len(eq_univ)))
+    G_bounds = get_bounds(weights=init_weights, LB=LB_i, UB=UB_i)
 
     opt_dict = { 'fun': lambda weights: get_ret_vol_mvutility(weights, d_ra=1, returns=ret_numpy, covar=covar_numpy)[2],
-                    'x0': init_weights,
-                'method': 'SLSQP',
-                'bounds': G_bounds,
-            'constraints': [g_cons, h_cons]}
+                  'x0': init_weights,
+              'method': 'SLSQP',
+              'bounds': G_bounds,
+         'constraints': [h_cons, g_cons]}
 
     try:
         opt_results = minimize(**opt_dict)
@@ -149,17 +150,18 @@ df_mv_allocation = pd.DataFrame(data=Q_W, index=df_ps.columns, columns=['MV Weig
 
 print(df_mv_allocation)
 
-plt.figure(figsize=(10, 6))
-plt.bar(x=df_mv_allocation.index, height=df_mv_allocation['MV Weights'])
-plt.title('Mean-variance Weights')
-plt.xlabel('Equities')
+#plt.figure(figsize=(10, 6))
+#plt.bar(data=df_mv_allocation['MV Weights'],x=df_mv_allocation.index, height=df_mv_allocation['MV Weights'])
+#plt.title('Mean-variance Allocation')
+#plt.xlabel('Equities')
 #plt.show()
-#-----------------------------------------------------------------------------------------------------------------------------------------------------#
-#--------------------#
-# Main - Risk Parity #
-#--------------------#
 
-# Risk Contribution
+#-----------------------------------------------------------------------------------------------------------------------------------------------------–-----------------------#
+#-----------------------#
+# Matplotlib Intermezzo #
+#-----------------------#
+
+# Risk Contribution, Relative Risk Contribution
 rc = []
 rrc = []
 for N, w in enumerate(Q_W):
@@ -171,11 +173,39 @@ for N, w in enumerate(Q_W):
     # Relative risk contribution
     rrc.append(rc_i/Q_variance**0.5)
 
-risk_contribution_df = pd.DataFrame(data=None, columns=None, index=None)
+risk_contribution_df = pd.DataFrame(data=None, columns=None, index=df_ps.columns)
 risk_contribution_df['RC'] = rc
 risk_contribution_df['RRC'] = rrc
+
+fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(10, 8), sharex=True, sharey=True)
+
+ax = axes[0]
+ax.set_ylim([0, 0.60])
+df_mv_allocation['MV Weights'].plot.bar(ax=ax, x=df_mv_allocation.index)
+tile_ax_string = r' Mean-Variance Allocation: $ \sigma = 0.25, \/ \sum_{i=0} ^N \omega_i = 1, 0 \leq \omega_i \leq 1 \/ \forall \/ i $'
+ax.set_title(size=14, label=tile_ax_string, y=1.25, pad=-27.5)
+xlocs, xlabs = plt.xticks()
+for i, v in enumerate(df_mv_allocation['MV Weights']):
+    ax.text(xlocs[i] - 0.225, v + 0.01, '{:.2f}%'.format(v*100))
+
+ax = axes[1]
+risk_contribution_df['RRC'].plot.bar(ax=ax,  x=risk_contribution_df.index, title='Relative Risk Contribution')
+ax.set_title(size=14, label=r'Relative Risk Contribution')
+xlocs, xlabs = plt.xticks()
+for i, v in enumerate(risk_contribution_df['RRC']):
+    ax.text(xlocs[i] - 0.225, v + 0.01, '{:.2f}%'.format(v*100))
+
+for tick in ax.get_xticklabels():
+    tick.set_rotation(45)
+
+plt.tight_layout(pad=2.5)
+#plt.show()
 
 print(risk_contribution_df)
 print(sum(rc))
 
+#-----------------------------------------------------------------------------------------------------------------------------------------------------–-----------------------#
 
+#--------------------#
+# Main - Risk Parity #
+#--------------------#
